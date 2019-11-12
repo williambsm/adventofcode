01846efc4ab77e7041f1c3e0835f6b181b9475fd
William Ste-Marie <williambsm@hotmail.fr>
2019-11-12 00:19:48 -0500

    Update 1

--- 2018/2/1.js
+++ 2018/2/1.js
@@ -1,39 +1,39 @@
 let input = "luojygedpvsthptkxiwnaorzmq lucjqgedppsbhftkxiwnaorlmq lucjmgefpvsbhftkxiwnaorziq lucjvgedpvsbxftkxiwpaorzmq lrcjygedjvmbhftkxiwnaorzmq lucjygedpvsbhftkxiwnootzmu eucjygedpvsbhftbxiwnaorzfq lulnygedpvsbhftkxrwnaorzmq lucsygedpvsohftkxqwnaorzmq lucjyaedpvsnhftkxiwnaorzyq lunjygedpvsohftkxiwnaorzmb lucjxgedpvsbhrtkxiwnamrzmq lucjygevpvsbhftkxcwnaorzma lucjbgedpvsbhftrxiwnaoazmq llcjygkdpvhbhftkxiwnaorzmq lmcjygxdpvsbhftkxswnaorzmq lucpygedpvsbhftkxiwraorzmc lucjbgrdpvsblftkxiwnaorzmq lucjfgedpvsbhftkxiwnaurzmv lucjygenpvsbhytkxiwnaorgmq luqjyredsvsbhftkxiwnaorzmq lucjygedpvavhftkxiwnaorumq gucjygedpvsbhkxkxiwnaorzmq lucjygedpvsbhftkxlwnaordcq lucjygedpvibhfqkxiwnaorzmm lucjegedpvsbaftkxewnaorzmq kucjygeqpvsbhfokxiwnaorzmq lugjygedwvsbhftkxiwnatrzmq lucjygedqvsbhftdxiwnayrzmq lucjygekpvsbuftkxiwnaqrzmq lucjygedpvsbhfbkxiwnaoozdq lscjygedpvzchftkxiwnaorzmq luckygedpvsbxftkxiwnaorvmq luyjygedgvsbhptkxiwnaorzmq lmcjygedpvsbhfckxiwnaodzmq lucmygedwvybhftkxiwnaorzmq lgcjhgedavsbhftkxiwnaorzmq lucjugedpvsbhftkxiwmaoozmq lucjygedpvybhftkxkwnaorumq lucjygedpvzbhfakxiwnaorzpq lucjygedpvsbhftyxzwnajrzmq lucjygedpvsdhfakxiwnoorzmq luyjygeopvhbhftkxiwnaorzmq lucjygadpvsbhntkxiwnaorzmx lucjygedzvsbhftkiiwuaorzmq sucjygodpvsbhftkxiwuaorzmq euijygydpvsbhftkxiwnaorzmq lucjlgeduvsbhftkxicnaorzmq lucjdgedpvsbhfgkxiwnhorzmq lucjymedpvsbhotkxiqnaorzmq lucjygmdpvsbhftkxywnairzmq lucjggedpvsbhfxkxiqnaorzmq sucjygedpvsbhftkxiwnaorjmv lucjlgedpvsbhftkxiwnairzmg lucjygedppubhftkxijnaorzmq lucjyxedpvsvhftkxlwnaorzmq lucjygedpvxbhftkfiwyaorzmq lucjygedposbhftkniwnaorzmw lucjygewpvsbhftgxiwnavrzmq lucjynedpvsbmftkaiwnaorzmq lucjyhedpvzbhftkxiwncorzmq lucjygedpvsbhfikpiwnaoezmq lupjypedpvsbhftkjiwnaorzmq lucjygudpvsbhfwkxivnaorzmq lucjygrdpvsbhatkxzwnaorzmq lucjbgmdpvsbhftkxihnaorzmq lucjmgedpvpbhftkxiwnaorcmq lucjygedpvskhfukmiwnaorzmq lucjygedgvsbhftkxiwnvprzmq lucjzgedppsbhytkxiwnaorzmq lfcjypedpvsbhftrxiwnaorzmq lucjyqldphsbhftkxiwnaorzmq lucjygedpvsbhftzxewnaorzqq lucjygeapvsbhftkxiinoorzmq lucjygedpvszhftguiwnaorzmq luojygedpvsbhftkxawnaornmq lucjygedpcsboetkxiwnaorzmq lufjygedpvfbhftaxiwnaorzmq luciygedpvsbhftkxhwaaorzmq lucjygedpvnbhftkaiwnaorzmc lucjygedpvsbhftkxiwcaorbdq lucjygelpvsbhftaxiwsaorzmq lujjygedpssbhftkxiwnaorzmr ludjygedpvsbhftkxiynaorzmj lukjygeedvsbhftkxiwnaorzmq lucjqpedpvsbhftkxiwnaozzmq jucjygedpvsbhftkxgwnaorqmq llwjygedpvsbhetkxiwnaorzmq rucjygedpvsbhftkxiwndorymq lucjygedpvsbhftvxswnaorwmq lucjygerpvsbhfykxiwnaormmq lucjynedpvsbhftkxijnaorziq ljcjygedpvrbhftkeiwnaorzmq lucjygedpnsbhftkxiwhaornmq lucjygadpvsbhftkxibnaorzqq lucjqgedpvsihftkxiwnaorzdq lucjygedpvsqhfttjiwnaorzmq llcjygedsvsbhftkxiwwaorzmq lfckygedpvsbhftkxiunaorzmq lucjyeedpdsbhftkxiwnaotzmq lucjygedpvsbhftkoiwnaoqzcq huwjvgedpvsbhftkxiwnaorzmq lucjygldpvsbdhtkxiwnaorzmq lycxygedpvsbhftmxiwnaorzmq lucjygedpvsbhftyxianvorzmq lucuygedpdsbhqtkxiwnaorzmq lucjyggdpvsbhftkxiwnavremq lucjyggdpvsbkftkxiwnaorbmq luchyqedpvsbhftixiwnaorzmq lpcnygedpvsbhftkxzwnaorzmq lucjygedpvsihftkxiwfaortmq lucjygvdpvsbhgtkxiwnamrzmq lucjygodpvrbhqtkxiwnaorzmq lucjygedpfsbhftkxipnaorzma lucjygedpvsbhftkxpcjaorzmq lucjygodbmsbhftkxiwnaorzmq lucjygedpvsbhftkxipnaogzmb luxjygjdpvsbhltkxiwnaorzmq lucxygedpvsbhftkxzwnaorjmq luajygedpvsbhftzxiwaaorzmq lhcjygedpvsqhftfxiwnaorzmq lucjygecphsbhftkxiwnaprzmq lucjygedpvsbhptkxifnaorqmq lucjygedpvichftkpiwnaorzmq lucjygedpcsbhstkxswnaorzmq kucjygedpvsbhftkxiwbyorzmq lfpjxgedpvsbhftkxiwnaorzmq lucjytldpvsbhftkxiwdaorzmq lufjygedpvfbhftbxiwnaorzmq lucjygebpvgbhftkxipnaorzmq luujygedpvdbhftkxiwnaorzmd lucjygedpvsbhfbyxwwnaorzmq lucjygedpvsbhftkxiwnaoqpmw qucgygedpvsbhftkxiwnaortmq ludjtgedpvsbhftkxiunaorzmq lucjyiedovsbhftkxiwjaorzmq lucjygedpysbjftoxiwnaorzmq lumjygedpvsbuftkxiknaorzmq lucjygedpvsbhfokxgonaorzmq lucjygeqpvsbhftkfiwnaorzeq lucjygedpvskhftkxiwntorkmq luujygedpvsbhftkxiwraorzmt lucwygedpvsbjftkxiwnaorzmj jucjyfedcvsbhftkxiwnaorzmq luujygedpnsehftkxiwnaorzmq lucjygedpvszhfckxiwnaorzmi lucjyredpvsbzftkpiwnaorzmq lucjygedpvsbwfgkxiwnaorzoq lucjygedpvgbhftkpiwnaorzms lucjygedpvjbhftkxzwnaoizmq vucjycedpvsbhftkxiwfaorzmq luawygeapvsbhftkxiwnaorzmq lucjygetpvsbhftkxiwnaafzmq lucjvgedpvsbhftkxywnavrzmq luolygedpvsbgftkxiwnaorzmq likjygedpvsbhftkxiwnabrzmq lucjygedovsbhftkxirpaorzmq lucjygedphsshftkxqwnaorzmq uuqjygewpvsbhftkxiwnaorzmq lucjygedcvsbhftkxiwoarrzmq lucnygedpvsbhfakxiwnaorzms lucjygedpvsbhntkxiwnawrzmb lucjygedpvsblfxkxivnaorzmq lucjygedpvsghftkxiwnaawzmq yucjygedpgsbhftkxiwnaorzbq lucjyweapvsbhftkxiwnaoezmq lucjygevpvsbyftcxiwnaorzmq luejygedovsbhftkxiwnqorzmq lucjyqedpvsbhfbkxiwnaorzms lucjypedpvsbhftwxiwnhorzmq lucjygedpvsbhmtkviwxaorzmq lucjogedpvpbhftkxiwnaorqmq lucjygedpvsbhztkxkwnaoazmq lucjyaedpvsbcftkxiwnaorzhq lucjygbdpvkbhftkxiznaorzmq lucpygedpvzbhftkxfwnaorzmq lucjmgedpcsbhftkxiwnaoezmq lucjygedyvsbbftkxiwnnorzmq lucjyyedpvsbhftuxiwnaonzmq lucjygfdpvsbhutkxiwnaorzmt uccjygedpvschftkxiwnaorzmq lusjygedpvbbhqtkxiwnaorzmq ducuygedpvsbhftkxiwnaorzyq lucjygkdvwsbhftkxiwnaorzmq cucjyyedpvsbhftkxiwnaerzmq lucjygedavsbhftkxiwnkorzbq lucjygedmvsyhftkxiwiaorzmq lucjygeipvsbhfpkxiwnaorzpq vucjugedvvsbhftkxiwnaorzmq lucjyzedpvsbhftkxpwnaoozmq lucjygedpvgbhftkxiwtaorzqq lecjygedpvcwhftkxiwnaorzmq lucjyghdpvsbhfcyxiwnaorzmq lucjygedpvesqftkxiwnaorzmq lucjyjehpvsbhftbxiwnaorzmq lucjygedpvtbhdtkxignaorzmq lucjygxdpgsbhftkxivnaorzmq lucjygvdpvsbhftkpiwnaorzqq lucjysedpvsbhftkxiwnalrzmc lucjygedpvkbhjtkxiwnaorsmq lucjygedpvsbvfgkxiwnaerzmq lucjygedpvsihftkxilnaorzmu lvcvygndpvsbhftkxiwnaorzmq lucjysedpqsbhftkxiwnaordmq lucsygeypvsbhftkwiwnaorzmq lucjygewpotbhftkxiwnaorzmq lucjysedpvsbhftkxiwnanrzmv lucjygedpvsbhutkxiwnaoplmq wucjygedpvsqbftkxiwnaorzmq lacjygeepvsbhftkxiwnjorzmq lucjygedpusyhftkxicnaorzmq qucjyredpvsbhftkxiwnworzmq lucjygedevsbhftkgiwnayrzmq lucjygedpksbrftkliwnaorzmq lucjygedpvsbhfgkxisnaorzeq lucjygedpvhdhftkeiwnaorzmq lucjsgedpvsboftkxiwnaorumq luctygedpvsbhftouiwnaorzmq lucjygedpvsjhfukjiwnaorzmq lucjagrepvsbhftkxiwnaorzmq lucjkgerpvsbhftkxiwnairzmq turjygedpvsbnftkxiwnaorzmq lbcjygedpvsbhftkdpwnaorzmq lucpygedpvsbhftkxnwnoorzmq jucjygedpvsbhbtkxicnaorzmq lecjygedpvsbhftkriwnaogzmq licjyvcdpvsbhftkxiwnaorzmq lrcjygewpnsbhftkxiwnaorzmq ltcxygedpvlbhftkxiwnaorzmq luctygedpvhbhztkxiwnaorzmq lucwygedplsbhfakxiwnaorzmq lucjygedpnsbhftkxiwjaoezmq lucpygedptsbhftkxiwnaorzmo lucjygedpvibhqtkxiknaorzmq lucjwgqdpvrbhftkxiwnaorzmq lucjmgkdpvsbhftkxiwraorzmq lucjygwupvsbhftkxiznaorzmq lucjhgedpvobhftkxiwncorzmq lucjygedpvsbhftkxiwnaohtmj lucjygedpvsbeftkfiwnaorzyq lucjygcdpvsbpftkhiwnaorzmq lucjygedpmsbhftkxiwnkouzmq oucjygedpvsbyftkximnaorzmq lucjcgedpvsbhftkxywnforzmq lfcjygedfvsbdftkxiwnaorzmq ducjygedevsbhfttxiwnaorzmq ldcjdgedpvsbhftkxiwnavrzmq lucjymedmvsbhqtkxiwnaorzmq lucjygedpvabhftkxiwnasrlmq lucjygefpvsbhftkxmwnaorkmq";
 
 function solution(input) {
-    let boxes = new Set(input.split(/ /g));
+    let boxes = input.split(/ /g);
 
     let twos = 0;
     let threes = 0;
 
-    boxes.forEach(function(box) {
+    for(let x= 0; x<boxes.length; x++) {
         let hasTwo = false;
         let hasThree = false;
 
         let letters = [];
 
-        for(x=0;x<box.length;x++) {
-            typeof letters[box[x]] === 'undefined' ? letters[box[x]] = 1 : letters[box[x]]++;
+        let word = boxes[x];
+
+        for(let y=0;y<word.length;y++) {
+            if (typeof letters[word[y]] === 'undefined') {
+                letters[word[y]] = 1
+            } else {
+                letters[word[y]]++;
+            }
         }
 
-        for(const letter in letters) {
-            if (letters[letter] === 2) {
+        for(const letter of Object.values(letters)) {
+            if (letter === 2 && !hasTwo) {
                 hasTwo = true;
+                twos++;
             }
 
-            if (letters[letter] === 3) {
+            if (letter === 3 && !hasThree) {
                 hasThree = true;
+                threes++;
             }
         }
-
-        if (hasTwo) {
-            twos++;
-        }
-
-        if (hasThree) {
-            threes++;
-        }
-    });
+    }
 
     return twos * threes;
 }
