cf7fa729b2756eaaa7949e6cd579956227da5d62
William Ste-Marie <williambsm@hotmail.fr>
2019-11-16 22:21:13 -0500

    5/2

--- 2018/5/1.js
+++ 2018/5/1.js
@@ -2,7 +2,7 @@
 
 function solution(input) {
     while (true) {
-        let index = input.search(/aA|Aa|bB|Bb|cC|Cc|dD|Dd|eE|Ee|fF|Ff|gG|Gg|hH|Hh|iI|Ii|jJ|Jj|kK|Kk|lL|Ll|mM|Mm|nN|Nn|oO|Oo|pP|Pp|qQ|Qq|rR|Rr|sS|Ss|tT|Tt|uU|Uu|vV|Vv|wW|Ww|xX|Xx|yY|Yy|zZ|Zz/g);
+        let index = input.search(/aA|Aa|bB|Bb|cC|Cc|dD|Dd|eE|Ee|fF|Ff|gG|Gg|hH|Hh|iI|Ii|jJ|Jj|kK|Kk|lL|Ll|mM|Mm|nN|Nn|oO|Oo|pP|Pp|qQ|Qq|rR|Rr|sS|Ss|tT|Tt|uU|Uu|vV|Vv|wW|Ww|xX|Xx|yY|Yy|zZ|Zz/);
 
         if (index !== -1) {
             input = input.slice(0, index) + input.slice(index + 2);
