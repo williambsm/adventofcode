12dee3df34fa7321b35d1c1b6f13db656df96da8
William Ste-Marie <williambsm@hotmail.fr>
2019-12-03 23:39:38 -0500

    2018/6

--- 2018/6/2.js
+++ 2018/6/2.js
@@ -3,13 +3,11 @@ const reader = require('../../reader.js');
 function solution(input) {
     input = input.split('\n');
     let coordinates = input.map(x => x.split(', ').map(Number));
-    let sizes = [];
     let gridX = new Set([]);
     let gridY = new Set([]);
+    let size = 0;
 
     for (let count = 0; count < coordinates.length; count = count + 1) {
-        sizes[count] = 0;
-
         gridX.add(coordinates[count][0]);
         gridY.add(coordinates[count][1]);
     }
@@ -21,33 +19,25 @@ function solution(input) {
 
     for (let x = minX; x <= maxX; x = x + 1) {
         for (let y = minY; y <= maxY; y = y + 1) {
-            let closestCoordinate = null;
-            let closestDistance = maxY;
-            let tied = false;
+            let maxDistance = 10000;
 
             for (let index = 0; index < coordinates.length; index = index + 1) {
                 let distanceBetween = Math.abs(coordinates[index][0] - x) + Math.abs(coordinates[index][1] - y);
 
-                if (distanceBetween === closestDistance) {
-                    tied = true;
-                } else if (distanceBetween < closestDistance) {
-                    closestCoordinate = index;
-                    closestDistance = distanceBetween;
-                    tied = false;
+                maxDistance = maxDistance - distanceBetween;
+
+                if (maxDistance < 0) {
+                    break;
                 }
             }
 
-            if (!tied && sizes[closestCoordinate] !== null) {
-                if (minX === x || maxX === x || minY === y || maxY === y) {
-                    sizes[closestCoordinate] = null;
-                } else {
-                    sizes[closestCoordinate]++;
-                }
+            if (maxDistance > 0) {
+                size++;
             }
         }
     }
 
-    return Math.max(...sizes);
+    return size;
 }
 
 reader.solve(solution);
