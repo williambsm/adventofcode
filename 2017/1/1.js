f53300a8d7f7111283cf153bda00d31f3d3a61b6
William Ste-Marie <williambsm@hotmail.fr>
2020-06-12 00:36:03 -0400

    Small update to 2017/1/1.js

--- 2017/1/1.js
+++ 2017/1/1.js
@@ -4,10 +4,10 @@ function solution(input) {
     let numbers = input.split('').map(Number);
     let sum = 0;
 
-    for (let x = 0; x < numbers.length; x = x + 1) {
-        if (x === numbers.length  - 1 && numbers[x] === numbers[0]) {
-            sum = sum + numbers[x];
-        } else if (numbers[x] === numbers[x + 1]) {
+    numbers.push(numbers[0]);
+
+    for (let x = 0; x < numbers.length - 1; x = x + 1) {
+        if (numbers[x] === numbers[x + 1]) {
             sum = sum + numbers[x];
         }
     }
