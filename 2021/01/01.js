1dbffec86f29314d4fd394950b7ac5c8d86f82c3
William Ste-Marie <williambsm@hotmail.fr>
2021-12-01 00:20:49 -0500

    2021-01-01

---
+++ 2021/01/01.js
@@ -0,0 +1,14 @@
+const reader = require('../../reader.js');
+
+function solution(input) {
+    let measurements = input.split('\n').map(Number);
+    let count = 0;
+
+    for (let i = 1; i < measurements.length; i = i + 1) {
+        if (measurements[i - 1] < measurements[i]) count++;
+    }
+
+    return count;
+}
+
+reader.solve(solution);
---
+++ 2021/01/02.js
@@ -0,0 +1,17 @@
+const reader = require('../../reader.js');
+
+function solution(input) {
+    let measurements = input.split('\n').map(Number);
+    let count = 0;
+
+    for (let i = 0; i < measurements.length - 3; i = i + 1) {
+        const a = measurements[i] + measurements[i+1] + measurements[i+2];
+        const b = measurements[i+1] + measurements[i+2] + measurements[i+3];
+
+        if (a < b) count++;
+    }
+
+    return count;
+}
+
+reader.solve(solution);
