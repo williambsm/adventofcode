8801ee3083f6346328255b1b63153e903326d035
William Ste-Marie <williambsm@hotmail.fr>
2021-12-02 00:20:06 -0500

    2021/2

---
+++ 2021/02/01.js
@@ -0,0 +1,24 @@
+const reader = require('../../reader.js');
+
+function solution(input) {
+    const directions = input.split('\n');
+    let x = 0;
+    let y = 0;
+
+    directions.forEach((direction) => {
+        direction = direction.split(' ');
+        direction[1] = parseInt(direction[1]);
+
+        if (direction[0] === 'forward') {
+            x += direction[1];
+        } else if (direction[0] === 'down') {
+            y += direction[1];
+        } else {
+            y -= direction[1];
+        }
+    });
+
+    return x * y;
+}
+
+reader.solve(solution);
---
+++ 2021/02/02.js
@@ -0,0 +1,26 @@
+const reader = require('../../reader.js');
+
+function solution(input) {
+    const directions = input.split('\n');
+    let x = 0;
+    let y = 0;
+    let aim = 0;
+
+    directions.forEach((direction) => {
+        direction = direction.split(' ');
+        direction[1] = parseInt(direction[1]);
+
+        if (direction[0] === 'forward') {
+            x += direction[1];
+            y = y + (aim * direction[1]);
+        } else if (direction[0] === 'down') {
+            aim += direction[1];
+        } else {
+            aim -= direction[1];
+        }
+    });
+
+    return x * y;
+}
+
+reader.solve(solution);
