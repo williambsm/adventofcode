75eea22150690c921c42ceaa7c75aaa9735e03a6
William Ste-Marie <williambsm@hotmail.fr>
2021-12-07 12:14:17 -0500

    2021/06

---
+++ 2021/06/01.js
@@ -0,0 +1,28 @@
+const reader = require('../../reader.js');
+
+function solution(input) {
+    let fishies = input.split(',').map(Number);
+    let days = [0,0,0,0,0,0,0,0,0];
+
+    fishies.forEach((fish) => {
+        days[fish]++;
+    });
+
+    for (let d = 0; d < 80; d = d + 1) {
+        days = advanceDays(days);
+    }
+
+    return days.reduce((sum, a) => sum + a, 0);
+}
+
+function advanceDays(days) {
+    let newDays = [0,0,0,0,0,0,days[0],0,days[0]];
+
+    for (let i = 1; i < days.length; i = i + 1) {
+        newDays[i-1] += days[i];
+    }
+
+    return newDays;
+}
+
+reader.solve(solution);
